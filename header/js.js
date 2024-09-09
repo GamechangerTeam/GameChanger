ba6148e147dd25a4e62ec1326bb661705154bab6
Crazy <crazy.kitaiets228@mail.ru>
2024-09-09 17:08:06 +0500

    fixed some mistakes

--- header/js.js
+++ header/js.js
@@ -1,11 +1,7 @@
 const burgerBtn  = document.querySelector(".burgerBtn")
-const header__list  = document.querySelector(".header__list")
-const headerBackground  = document.querySelector(".header--background")
-const contactWrapper  = document.querySelector(".contact-wrapper")
+const header  = document.querySelector(".header")
 
 burgerBtn.addEventListener("click", (e) => {
   burgerBtn.classList.toggle("open")
-  header__list.classList.toggle("open")
-  headerBackground.classList.toggle("open")
-  contactWrapper.classList.toggle("open")
+  header.classList.toggle("open")
 })
