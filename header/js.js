5ae9bbe4f76e7fbbf0ff0857fa5d827e94c9833c
Crazy <crazy.kitaiets228@mail.ru>
2024-09-09 21:18:02 +0500

    added styles for play button

--- header/js.js
+++ header/js.js
@@ -1,7 +1,12 @@
-const burgerBtn  = document.querySelector(".burgerBtn")
-const header  = document.querySelector(".header")
+const burgerBtn = document.querySelector(".burgerBtn");
+const header = document.querySelector(".header");
+const copy_number = document.querySelector(".copy_number");
 
 burgerBtn.addEventListener("click", (e) => {
-  burgerBtn.classList.toggle("open")
-  header.classList.toggle("open")
-})
+  burgerBtn.classList.toggle("open");
+  header.classList.toggle("open");
+});
+
+copy_number.addEventListener("click", () => {
+  navigator.clipboard.writeText(copyText.value);
+});
